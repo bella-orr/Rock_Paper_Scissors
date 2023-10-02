a562379d763d6e91d5c27bd3b06a1a156a3446d6
bella-orr <143103302+bella-orr@users.noreply.github.com>
2023-10-02 11:17:21 -0400

    Initial commit

---
+++ src/RockPaperScissorsFrame.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+public class RockPaperScissorsFrame extends JFrame
+{
+    //JPanels
+    JPanel buttonPanel;
+    JPanel statsPanel;
+    JPanel resultsPanel;
+
+    //JButtons;
+    JButton rockBttn;
+    JButton paperBttn;
+    JButton scissorsBttn;
+    JButton quitBttn;
+
+    JTextArea results;
+    JScrollPane scroller;
+
+    
+}
